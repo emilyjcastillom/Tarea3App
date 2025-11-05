2213a19975f5c16ae37b052a4d434e3dabd09247
Emily <a00839559@tec.mx>
2025-11-05 14:26:31 -0600

    initial commit

---
+++ Tarea3App/Tarea3AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea3AppApp.swift
+//  Tarea3App
+//
+//  Created by Alumno on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Tarea3AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
